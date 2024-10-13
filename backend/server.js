890582386afd3b57da09ea9b316718acfd5a805c
Tyler Wade <tylerwade@Mac.hitronhub.home>
2024-10-13 18:38:23 -0400

    1 - auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 
+// Middleware
+app.use(express.json());    // to parse req.body
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// Routing
 app.use("/api/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
