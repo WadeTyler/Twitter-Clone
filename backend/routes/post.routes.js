951ac30432e274716188995537f887e8e01e5d13
Tyler Wade <tylerwade@Mac.hitronhub.home>
2024-10-13 22:48:30 -0400

    1 - post routes and controllers added

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { getAllPosts, getFollowingPosts, getUserPosts, getLikedPosts, createPost, deletePost, commentOnPost, likeUnlikePost } from '../controllers/post.controller.js';
+const router = express();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost );
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-
+// Packages
 import express from "express";
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
 
+// Routes
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 const app = express();
 dotenv.config();
@@ -25,6 +27,7 @@ app.use(cookieParser());
 // Routing
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 
 const PORT = process.env.PORT || 8000;
