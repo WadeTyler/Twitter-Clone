341918dd896f516c9682fe1b73b13fd4be85db2f
Tyler Wade <tylerwade@Mac.hitronhub.home>
2024-10-13 21:40:53 -0400

    1 - user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 const app = express();
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 // Middleware
 app.use(express.json());    // to parse req.body
@@ -14,6 +24,7 @@ app.use(cookieParser());
 
 // Routing
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 
 const PORT = process.env.PORT || 8000;
